fe8e7dcdca5604d95b4eba7698f441888065074a
Justin Chazen <Justinchazen96@gmail.com>
2022-04-07 18:01:42 -0400

    Added posts request

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,7 +1,21 @@
+import { useEffect, useState } from 'react';
+
 const Timeline = () => {
+    const [posts, setPosts] = useState([])
+
+    useEffect(async () => {
+        console.log('Rendering Timeline ...')
+        let req = await fetch('https://raw.githubusercontent.com/rmdashrfv/lifeinvader/main/src/data/posts.json')
+        let res = await req.json()
+        setPosts(res)
+    }, [])
+
     return (
         <div>
             <h2>Timeline</h2>
+            {
+                posts.map((element) => <h1>{element.id}</h1>)
+            }
         </div>
     )
 }
