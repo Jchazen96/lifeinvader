376f9e78c52bb1238e0423c70f7a9b4c268f5f9c
Justin Chazen <Justinchazen96@gmail.com>
2022-04-08 14:34:02 -0400

    added Navbar component

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,10 +1,12 @@
 import Timeline from "./Timeline";
 import Sidebar from "../Sidebar";
+import Navbar from "./Navbar";
+
 
 const Homepage = () => {
     return (
         <div>
-            
+            <Navbar />
             <Timeline />
             <Sidebar />
         </div>
